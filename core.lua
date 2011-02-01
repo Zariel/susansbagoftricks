33f48ca459e06a702a3b13a97290de851ad55506
Chris Bannister <c.bannister@gmail.com>
2011-02-01 14:02:57 +0000

    Add more spells, use cosine interpolation

--- core.lua
+++ core.lua
@@ -23,47 +23,18 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.]]
 
-local GetTime = GetTime
-local GetSpellCooldown = GetSpellCooldown
-local GetSpellName = GetSpellName
-local pi = math.pi/2
-local math_sin = math.sin
-
-local playerName = UnitName("player")
-
-local addon = CreateFrame("Frame", nil, UIParent)
-local t = addon:CreateTexture(nil, "OVERLAY")
-
-local size = 175
-local alpha = 0.85
-
-t:SetHeight(size)
-t:SetWidth(size)
-t:SetPoint("CENTER", UIParent, "CENTER")
-t:SetAlpha(0)
-t:SetTexCoord(0.07, 0.93, 0.07, 0.93)
-t:Hide()
-
-addon.icon = t
-
-addon.queue = {}
-addon.running = false
-addon.watched = {}
-addon.timer = 0
-
-addon.spells = {}
-
-local spells = {
-	undead = {
-		-- Undead
+local temp = {
+	Undead = {
 		"Will of the Forsaken",
 		"Cannibalize",
 	},
 	NightElf = {
 		"Shadowmeld",
 	},
+	Human = {
+		"Every Man for Himself",
+	},
 	Rogue = {
-		-- Rogue
 		"Riposte",
 		"Gouge",
 		"Blade Flurry",
@@ -97,9 +68,11 @@ local spells = {
 		"Explosive Shot",
 		"Aimed Shot",
 		"Scare Beast",
-		"Tranqulizing Shot"
+		"Tranqulizing Shot",
+		"Black Arrow",
+		"Master's Call",
 	},
-	warrior = {
+	Warrior = {
 		"Charge",
 		"Intercept",
 		"Overpower",
@@ -110,25 +83,82 @@ local spells = {
 		"Shield Bash",
 		"Shield Block",
 	},
+	Paladin = {
+		"Judgment of Light",
+		"Judgment of Wisdom",
+		"Crusader Strike",
+		"Holy Shock",
+		"Lay on Hands",
+		"Avenger's Shield",
+		"Ardent Defender",
+		"Hammer of Righteous",
+		"Hammer of Justice",
+		"Hand of Reckoning",
+		"Guardian of the Anchient Kings",
+		"Avenging Wrath"
+	},
+	Shaman = {
+		"Thunderstorm",
+		"Chain Lightning",
+		"Frost Shock",
+		"Earth Shock",
+		"Wind Shear",
+		"Elemental Mastery",
+	},
 }
 
-if spells[UnitClass("player")] then
-	addon.spells = {}
-	for k, spell in pairs(spells[UnitClass("player")]) do
-		addon.spells[spell] = true
+local spells = {}
+do
+	local class, race = UnitClass("player"), UnitRace("player")
+	for type, v in pairs(temp) do
+		if(type == class or type == race) then
+			for i, spell in pairs(v) do
+				spells[spell] = true
+			end
+		end
 	end
+
+	if(not next(spells)) then return end
 end
 
-if spells[UnitRace("player")] then
-	addon.spells = addon.spells or {}
-	for k, spell in pairs(select(2, spells[UnitRace("player")])) do
-		addon.spells[spell] = true
-	end
+
+local addon = CreateFrame("Frame", nil, UIParent)
+local t = addon:CreateTexture(nil, "OVERLAY")
+
+local GetTime = GetTime
+local GetSpellCooldown = GetSpellCooldown
+local GetSpellInfo = GetSpellInfo
+local pi = math.pi
+local cos = math.cos
+local select = select
+
+local playerName = UnitName("player")
+
+local size = 175
+local alpha = 0.85
+
+local steps = 50
+local modifier = 1 / steps
+
+-- Taken from oPanel ~~
+local cos = math.cos
+local cosineInterpolation = function(y1, y2, mu)
+	return y1 + (y2 - y1) * (1 - cos(pi * mu)) / 2
 end
 
-if not addon.spells then return end
+t:SetHeight(size)
+t:SetWidth(size)
+t:SetPoint("CENTER", UIParent, "CENTER")
+t:SetAlpha(0)
+t:SetTexCoord(0.07, 0.93, 0.07, 0.93)
+t:Hide()
 
-spells = nil
+addon.icon = t
+
+addon.queue = {}
+addon.running = false
+addon.watched = {}
+addon.timer = 0
 
 addon:SetScript("OnEvent", function(self, event, ...)
 	return self[event](self, ...)
@@ -139,56 +169,74 @@ addon:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 
 local cast = {}
 
+local texCache = setmetatable({}, {
+	__mode = "k",
+	__index = function(self, spell)
+		local text = select(3, GetSpellInfo(spell))
+		rawset(self, spell, text)
+		return text
+	end,
+})
+
 function addon:COMBAT_LOG_EVENT_UNFILTERED(timeStamp, event, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags, spellId, spellName)
-	if sourceName == playerName and spellName and self.spells[spellName] then
+	if(sourceName == playerName and spellName and spells[spellName]) then
 		cast[spellName] = true
 	end
 end
 
 function addon:SPELL_UPDATE_COOLDOWN()
 	local start, duration, enabled
-	for spell in pairs(self.spells) do
+	for spell in next, cast do
 		start, duration, enabled = GetSpellCooldown(spell)
-		if enabled == 1 and duration > 1.5 and cast[spell] then
+
+		if(enabled == 1 and duration > 1.5) then
 			self.watched[spell] = start + duration
 		end
 	end
 end
 
+local time
+local cosine = 0
+local step = 0
+
 addon:SetScript("OnUpdate", function(self, elapsed)
 	-- Update watched timers
-	local time = GetTime()
-	for spell, finish in pairs(self.watched) do
-		if time > finish then
-			table.insert(self.queue, spell)
+	-- this can cause problems if a spell comes off cooldown before we expect
+	-- it, ie explosion shot from Lock N Load
+	time = GetTime()
+	for spell, finish in next, self.watched do
+		-- Pre empt faster plx
+		if(time >= (finish - 0.5)) then
+			self.queue[#self.queue + 1] = { spell, time }
+
 			self.watched[spell] = nil
 			cast[spell] = nil
 		end
 	end
 
 	-- Are we running allready?
-	if self.running then
-		self.timer = self.timer + (elapsed * 1.35)
-		local s = math_sin(self.timer)
-		if self.timer > pi then
-			self.icon:SetAlpha(0)
+	if(self.running) then
+		if(step > steps) then
 			self.icon:Hide()
+			self.icon:SetAlpha(0)
 			self.icon:SetHeight(size)
 			self.icon:SetHeight(size)
-			self.timer = 0
 			self.running = false
+			step = 0
 		else
-			self.icon:SetHeight(s * size)
-			self.icon:SetWidth(s * size)
-			self.icon:SetAlpha(s * alpha)
+			cosin = cosineInterpolation(0, 1, modifier * step)
+			self.icon:SetHeight(cosin * size)
+			self.icon:SetWidth(cosin * size)
+			self.icon:SetAlpha(cosin * alpha)
+			step = step + 1
 			return
 		end
 	end
 
-	if #self.queue == 0 then return end
-
-	local spell = table.remove(self.queue, 1)
-	self.icon:SetTexture(select(3, GetSpellInfo(spell)))
+	if(#self.queue == 0) then return end
+	table.sort(self.queue, function(a, b) return a[2] < b[2] end)
+	local spell = table.remove(self.queue, 1)[1]
+	self.icon:SetTexture(texCache[spell])
 	self.running = true
 	self.icon:Show()
 end)
